d72cf3c03a4ed0eca334f23cb8f3459e556fb9cc
DeepBlue <deepblue5991@gmail.com>
2024-06-20 23:23:35 +0300

    chore: Update image paths and remove unused code

--- script/script.js
+++ script/script.js
@@ -2,10 +2,9 @@
 let i = 0;
 setInterval(function () {
   if (i%2) {
-    document.getElementById("it").href = "/resource/images/logo old3.2.png";
-    i++;
+    document.getElementById("it").href = "resource/images/icons/logo old3.2.png";
   } else {
-    document.getElementById("it").href = "/resource/images/logo old4.2.png";
-    i++;
+    document.getElementById("it").href = "resource/images/icons/logo old4.2.png";
   }
+  i = (i%2) + 1;
 }, 1000);
